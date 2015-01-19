d3312e5b5e92e3e5c666697cc8a5d77ba55614a4
Say LY <sly@student.42.fr>
2015-01-19 03:26:37 +0100

    feat:parser // read options //

--- include/ft_ls.h
+++ include/ft_ls.h
@@ -6,12 +6,13 @@
 /*   By: sly <sly@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/17 05:04:10 by sly               #+#    #+#             */
-/*   Updated: 2015/01/17 05:05:36 by sly              ###   ########.fr       */
+/*   Updated: 2015/01/19 01:48:15 by sly              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #	ifndef FT_LS_H
 #define FT_LS_H
+#define OPTION_NB 5
 
 void		hello(void);
 #	endif
--- src/ft_ls.c
+++ src/ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: sly <sly@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/17 22:33:03 by sly               #+#    #+#             */
-/*   Updated: 2015/01/18 05:26:23 by sly              ###   ########.fr       */
+/*   Updated: 2015/01/19 02:11:45 by sly              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,36 @@ static void		ft_errorMessage(char c)
 	ft_putstr_fd("\nusage: ls [-Ralrt] [file ...]\n", 2);
 }
 
-static int		ft_isOption(char c)
+static int		ft_addOption(char **options, char c)
 {
-	return ((c == 'a') || (c == 'l') || (c == 'r') || (c == 'R') || (c == 't'));
+	char		*s;
+
+	if (!(s = (char*)malloc(sizeof(char) * 2)))
+		return (-1);
+	s[0] = c;
+	s[1] = '\0';
+	ft_strcat(*options, s);
+	return (0);
 }
 
-static int		ft_readOption(char *arg)
+static int		ft_isOption(char c, char **options)
+{
+	int			i;
+
+	if ((c == 'a') || (c == 'l') || (c == 'r') || (c == 'R') || (c == 't'))
+	{
+		i = 0;
+		while ((*options)[i])
+			if ((*options)[i++] == c)
+				return (1);
+		if (ft_addOption(options, c) == -1)
+			return (-1);
+		return (1);
+	}
+	return (0);
+}
+
+static int		ft_readOption(char *arg, char **options)
 {
 	size_t		i;
 	size_t		len;
@@ -36,7 +60,7 @@ static int		ft_readOption(char *arg)
 	len = ft_strlen(arg);
 	while (++i < len)
 	{
-		ret = ft_isOption(arg[i]);
+		ret = ft_isOption(arg[i], options);
 		if (ret == 0)
 		{
 			if (i == 1 && arg[i] == '-' && arg[i + 1] == '\0')
@@ -44,11 +68,13 @@ static int		ft_readOption(char *arg)
 			ft_errorMessage(arg[i]);
 			return (-1);
 		}
+		else if (ret == -1)
+			return (-1);
 	}
 	return (0);
 }
 
-static int		ft_readArg(int argc, char **argv)
+static int		ft_readArg(int argc, char **argv, char **options)
 {
 	int			i;
 	int			ret;
@@ -56,23 +82,35 @@ static int		ft_readArg(int argc, char **argv)
 	i = 2;
 	while (i <= argc && argv[i - 1][0] == '-')
 	{
-		ret = ft_readOption(argv[i++ - 1]);
+		ret = ft_readOption(argv[i++ - 1], options);
 		if (ret == -1)
 			return (-1);
 	}
-	if (argc - i > 0)
+	if (argc - i >= 0)
 		ft_putstr("ok\n");
 	else
 		ft_putstr(".\n");
 	return (0);
 }
 
+static void		ft_initOptions(char **options)
+{
+	*options = (char*)malloc(sizeof(char) * (OPTION_NB + 1));
+	bzero(*options, OPTION_NB);
+}
+
 int				main(int argc, char **argv)
 {
 	int			ret;
+	char		*options;
 
-	ret = ft_readArg(argc, argv);
-	if (ret == -1)
+	ft_initOptions(&options);
+	if ((ret = ft_readArg(argc, argv, &options)) == -1)
+	{
+		ft_putstr("ft_readArg error, abort\n");
 		return (1);
+	}
+	printf("options:%s\n", options);
+	//printf("verification options\nl_opt=%d\nR_opt=%d\na_opt=%d\nr_opt=%d\nt_opt=%d\n", options.l_opt, options.R_opt, options.a_opt, options.r_opt, options.t_opt);
 	return (0);
 }
