2e77c0e43d4841e5da5fd4cff980402169bc9f8c
Say LY <sly@student.42.fr>
2015-01-29 22:11:29 +0100

    feat:dirlst // dirlst better management with int tab //

--- src/ft_run.c
+++ src/ft_run.c
@@ -6,7 +6,7 @@
 /*   By: sly <sly@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/25 11:26:10 by sly               #+#    #+#             */
-/*   Updated: 2015/01/27 21:31:51 by sly              ###   ########.fr       */
+/*   Updated: 2015/01/29 22:07:51 by sly              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 #include <sys/errno.h>
 #include <stdio.h>
 
-/*static int				ft_openTestDir(char *path, struct stat **buf)
+static int				ft_getStat(char *path, struct stat **buf)
 {
 	int					ret;
 
@@ -26,9 +26,9 @@
 		return (-1);
 	//printf("mode of %s:%d\n", path, (*buf)->st_mode);
 	return (0);
-}*/
+}
 
-static int				ft_openDir(char *path)
+/*static int				ft_openDir(char *path)
 {
 	DIR					*stream;
 	struct dirent		*dirent;
@@ -41,7 +41,7 @@ static int				ft_openDir(char *path)
 	if ((ret = closedir(stream)) == -1)
 		return (-1);
 	return (0);
-}
+}*/
 
 /*static int			ft_recursiveRun(char *path)
   {
@@ -87,56 +87,54 @@ void					ft_run(int argc, char **argv, int i, char *options)
 	/*
 	 *	dirLst[0]: list of operands which are not a file nor a directory
 	 *	dirLst[1]: list of files and directories
-	 *	dirLst[2]: list of directories
+	 *	temp[0]: count
+	 *	temp[1]: isdirLst[0]
+	 *	temp[2]: isdirLst[1]
 	 */
-	t_dir				*dirLst[3];
-	int					temp;
-	//struct stat			*dirRaw;
+	t_dir				*dirLst[2];
+	int					temp[3];
+	struct stat			*dirRaw;
 
-	temp = 0;
-	while (temp < 3)
-		dirLst[temp++] = NULL;
-	temp = i;
-	if (argc >= temp)
-		while (argc >= temp)
+	temp[0] = 0;
+	temp[1] = 0;
+	temp[2] = 0;
+	while (temp[0] < 2)
+		dirLst[(temp[0])++] = NULL;
+	temp[0] = i;
+	if (argc >= temp[0])
+		while (argc >= (temp[0])++)
 		{
-			//if (ft_openTestDir(argv[temp++ - 1], &dirRaw) == -1)
-			if (ft_openDir(argv[temp++ - 1]) == -1)
+			if (ft_getStat(argv[temp[0] - 2], &dirRaw) == -1)
 			{
-				if (errno == 2)
-					ft_dirAdd(&dirLst[0], ft_dirnew(-1, argv[temp - 2]));
-				else
-				{
-					printf("ok\n");
-					ft_dirAdd(&dirLst[1], ft_dirnew(-1, argv[temp - 2]));
-					ft_dirAdd(&dirLst[2], ft_dirnew(-1, argv[temp - 2]));
-				}
-				//dirLst[0] = ft_dirnew(-1, argv[temp - 2]);
-				//printf("adresse de dirLst[0]:%p\n", dirLst[0]);
-				/*while (dirLst[0])
-				{
-					ft_putstr(ft_itoa(dirLst[0]->mode));
-					ft_putstr(" ");
-					ft_errorNonDirectoryOperand(dirLst[0]->name);*/
-					/*printf("dirLst ordonne:%s\n", dirLst[0]->name);
-					dirLst[0] = dirLst[0]->next;
-					free(dirLst[0]);
-				}*/
+				printf("fail to open:%s, errno:%d\n", argv[temp[0] - 2], errno);
+				ft_dirAdd(&dirLst[0], ft_dirnew(-1, argv[temp[0] - 2]));
+				temp[1] = 1;
 			}
-			/*else
+			else
 			{
-				dirLst[1] = ft_dirnew(dirRaw->st_mode, argv[temp - 2]);
+				printf("argv:%s\n", argv[temp[0] - 2]);
+				ft_dirAdd(&dirLst[1], ft_dirnew(dirRaw->st_mode, argv[temp[0] - 2]));
 				//int i = ((dirLst[1]->mode) & (1 << 14)) > 0;
-				printf("Directory test:%d," mode value:%d,  operand name:%s\n", i, dirLst[1]->mode, dirLst[1]->name);
-			}*/
+				printf("Directory test:%d, mode value:%d, operand name:%s\n", temp[0] - 2, dirLst[1]->mode, dirLst[1]->name);
+				temp[2] = 1;
+			}
+				//printf("Directory test:%d, mode value:%d,  operand name:%s\n", temp[0] - 2, dirLst[1]->next->mode, dirLst[1]->next->name);
 		}
 	else
 	{
 		ft_putendl(".");
-		//ft_openTestDir(".", &dirRaw);
+		//ft_getStat(".", &dirRaw);
 	}
-	ft_sort(&dirLst[0]);
-	free(dirLst[0]);
+	if (temp[1] == 1)
+	{
+		ft_sort(&dirLst[0]);
+		free(dirLst[0]);
+	}
+	if (temp[2] == 1)
+	{
+		ft_sort(&dirLst[1]);
+		free(dirLst[1]);
+	}
 	while (*options)
 		options++;
 }
